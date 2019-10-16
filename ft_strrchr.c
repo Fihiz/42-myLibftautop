501d82613f0cf84eee0671917113e5348a5079ee
Salome D'audeteau <sad-aude@z1r2p1.le-101.fr>
2019-10-16 18:40:21 +0200

    ft_strrchr seems to be ok

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,30 +1,38 @@
 /* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
-/*   ft_strchr.c                                      .::    .:/ .      .::   */
+/*   ft_strrchr.c                                        .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: sad-aude <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2019/10/16 11:41:54 by sad-aude     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/16 18:36:32 by sad-aude    ###    #+. /#+    ###.fr     */
+/*   Created: 2019/10/16 18:07:43 by sad-aude     #+#   ##    ##    #+#       */
+/*   Updated: 2019/10/16 18:37:56 by sad-aude    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strrchr(const char *s, int c)
 {
 	int i;
+	int len;
 
 	i = 0;
 	while (s[i])
 	{
-		if (s[i] == c)
-			return ((char *)s + i); //+ i pour afficher a partir de l'occurence
+		i++;
+	}
+	len = i;
+	i = 0;
+	while (s[i])
+	{
+		if (s[len] == c)
+			return ((char *)s + len);
+		len--;
 		i++;
 	}
 	if (c == 0)
-		return ((char *)s + i);
+		return ((char *)s + len);
 	return (0);
 }
--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int		main()
 {
 	char str[] = "Hello World";
 
-	printf("Looking for a w in %s\n", ft_strchr(str, 'W'));
-	printf("Looking for a w with the true function %s\n", strchr(str, 'W'));
+	printf("Looking for a w in %s\n", ft_strrchr(str, 'o'));
+	printf("Looking for a w with the true function %s\n", strrchr(str, 'o'));
 	return (0);
 }
