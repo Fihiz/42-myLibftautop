f1da0d05c0f3a75cf9769b58b51a4469e2834b7a
Salome D'audeteau <sad-aude@z1r4p4.le-101.fr>
2019-10-21 18:32:05 +0200

    adding the simple one

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 #include "libft.h"
 #include <stdio.h>
 
-int		main()
+/* int		main()
 {
 	char str[] = "";
 	char str2[] = "Hello";
 
 	printf("Looking with mine %d\n", ft_strncmp(str, str2, 6));
 	printf("Looking with the real one %d\n", strncmp(str, str2, 6));
+	return (0);
+} */
+
+int		main()
+{
+	ft_putnbr_fd(-2, 1);
 	return (0);
 }
