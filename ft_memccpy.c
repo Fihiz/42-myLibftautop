45a70b6c8c7e5e795749161cec1abd3faaa419f2
Salome D'audeteau <sad-aude@z1r5p1.le-101.fr>
2019-10-22 17:18:40 +0200

    trying to fix

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: sad-aude <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/15 14:28:25 by sad-aude     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/22 14:32:48 by sad-aude    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/22 15:19:14 by sad-aude    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -22,7 +22,7 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 	i = 0;
 	str = src;
 	desti = dest;
-	while (i < n && i != c)
+	while (i < n && i != (unsigned char)c)
 	{
 		desti[i] = str[i];
 		i++;
