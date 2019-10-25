9cdfd5b37110faa918e52619932526be515a1521
Salome D'audeteau <sad-aude@z1r6p1.le-101.fr>
2019-10-25 19:49:44 +0200

    last push of the day

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,9 +6,35 @@
 /*   By: sad-aude <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/24 14:26:40 by sad-aude     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/24 14:26:55 by sad-aude    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/25 18:28:50 by sad-aude    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+int		ft_atoi(const char *str)
+{
+	int i;
+	int signe;
+	int nb;
+
+	i = 0;
+	signe = 1;
+	nb = 0;
+	while (str[i] == '\f' || str[i] == '\t' || str[i] == '\n' || str[i] == '\r'
+			|| str[i] == '\v' || str[i] == ' ')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			signe *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nb = (nb * 10) + (str[i] - '0');
+		i++;
+	}
+	return (nb * signe);
+}
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: sad-aude <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/15 13:54:55 by sad-aude     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/24 15:18:57 by sad-aude    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/25 18:00:46 by sad-aude    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -28,5 +28,5 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 		i++;
 	}
 	desti[i] = str[i];
-	return ((void*)desti);
+	return (desti);
 }
