dd281d46bb5e5c1614cebc01f034cb8bd22eccd2
Salome D'audeteau <sad-aude@z1r6p1.le-101.fr>
2019-11-08 19:39:39 +0100

    come back

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: sad-aude <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/15 14:44:33 by sad-aude     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/30 17:21:38 by sad-aude    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/08 19:23:30 by sad-aude    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,13 +15,13 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	const char	*str;
-	char		*desti;
-	char		*temp;
-	size_t		i;
+	const unsigned char		*str;
+	unsigned char			*desti;
+	unsigned char			*temp;
+	size_t					i;
 
 	i = 0;
-	str = (const char*)src;
+	str = (const unsigned char*)src;
 	desti = dest;
 	temp = 0;
 	while (i < n)
