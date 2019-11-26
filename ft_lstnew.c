44c5f0841a27db0e2baf8df2e8e5a9ef803f3eb6
Salome D'audeteau <sad-aude@z1r5p1.le-101.fr>
2019-11-26 21:19:35 +0100

    beginning bonus functions

---
+++ ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_lstnew.c                                      .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: sad-aude <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/11/26 20:39:32 by sad-aude     #+#   ##    ##    #+#       */
+/*   Updated: 2019/11/26 21:19:15 by sad-aude    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*data;
+
+	if(!(data = malloc(sizeof(t_list))))
+		return (NULL);
+	if (content)
+		 data->content = content;
+	return (data);
+}
